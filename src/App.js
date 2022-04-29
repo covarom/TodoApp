c23226806582e096961ad5d00056a9f26a039c82
covarom <huudoan.qna@gmail.com>
2022-04-29 23:25:20 +0700

    90% =))

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import { useCallback, useEffect, useState } from "react";
+import { v4}  from 'uuid';
+
 
 function App() {
+  //Set for TodoList
+  const [todoList,setTodoList]=useState([]);
+
+  //Set for TextInput 
+  const [textInput,settextInput]=useState("");
+
+  const onTextInputChange= useCallback((e) =>{
+    settextInput(e.target.value);
+  },[] );
+
+  const onAddBtnClick = useCallback((e) =>{
+    // Add textInput to TodoList
+    setTodoList([{id: v4(),name:textInput,isCompleted: false},...todoList])
+    settextInput("");
+  },[textInput,todoList]);
+
+  //method to check competed 
+  const onCheckBtnClick = useCallback((id) =>{
+    setTodoList(prevState => prevState.map(todo => todo.id ===id?{...todo, isCompleted:true} : todo))
+  },[])
+
+  const TODO_APP_STORAGE_KEY="TODO_APP";
+
+ //get data from local storage
+ useEffect(() => {
+  const storageTodoList=localStorage.getItem(TODO_APP_STORAGE_KEY);
+  if(storageTodoList){
+    setTodoList(JSON.parse(storageTodoList));
+  }
+},[])
+
+  // save todoList to local storage
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY,JSON.stringify(todoList));
+  },[todoList]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>List need to do:</h3>
+      <Textfield name="add-todo" placeholder="Add to List ..." elemAfterInput={
+        <Button isDisabled ={!textInput} appearance='primary' onClick={onAddBtnClick} >Add</Button>
+      }
+      css={{padding:"2px 4px 2px"}}
+      value={textInput}
+      onChange={onTextInputChange}
+      ></Textfield>
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick}/>
+    </>
+  )
 }
 
 export default App;
